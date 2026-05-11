bb66e105e1bca144907176073f8a824c10987717
bp <86982605+benpages@users.noreply.github.com>
2026-05-11 14:28:06 +0200

    installed eleventy

---
+++ .eleventy.js
@@ -0,0 +1,33 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("./src/css");
+	eleventyConfig.addPassthroughCopy("./src/js");
+	eleventyConfig.addPassthroughCopy("./src/assets");
+	eleventyConfig.addPassthroughCopy("src/_redirects");
+
+	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+	eleventyConfig.addFilter("postDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+	});
+
+	// Add a filter to format dates for structured data
+	eleventyConfig.addFilter("isoDate", function (date) {
+		return date.toISOString();
+	});
+
+	eleventyConfig.addFilter(
+		"excludeFromCollection",
+		(collection = [], pageUrl = this.ctx.page.url) => {
+			return collection.filter((post) => post.url !== pageUrl);
+		},
+	);
+
+	return {
+		dir: {
+			input: "src",
+			output: "public",
+		},
+	};
+};
